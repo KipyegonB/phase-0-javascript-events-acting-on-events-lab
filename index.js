c6bde3dd15522ef87d90eb967eac9423bc7693e8
KipyegonB <koechbernard08@gmail.com>
2022-08-17 15:10:27 +0300

    Completed assisgment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code here
+function moveDodgerLeft(){
+    dodger.style.left = `20px`;
+}
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) { dodger.style.left = `${left + 1}px`;
+}
+}
